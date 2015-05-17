9198a0a2b380ee3a363af1c8de1e24c678fb8c66
Patrick Huber <stackmagic@gmail.com>
2015-05-17 11:01:27 +0200

    #1 add new fields to hash index

--- src/main/java/net/swisstech/arangodb/model/idx/HashIndex.java
+++ src/main/java/net/swisstech/arangodb/model/idx/HashIndex.java
@@ -3,8 +3,27 @@ package net.swisstech.arangodb.model.idx;
 /** see https://docs.arangodb.com/IndexHandling/Hash.html */
 public class HashIndex extends AbstractIndex {
 
+	private boolean sparse;
+	private long selectivityEstimate;
+
 	@Override
 	public String getType() {
 		return IndexTypes.HASH;
+	}
+
+	public boolean isSparse() {
+		return sparse;
+	}
+
+	public void setSparse(boolean sparse) {
+		this.sparse = sparse;
+	}
+
+	public long getSelectivityEstimate() {
+		return selectivityEstimate;
+	}
+
+	public void setSelectivityEstimate(long selectivityEstimate) {
+		this.selectivityEstimate = selectivityEstimate;
 	}
 }
